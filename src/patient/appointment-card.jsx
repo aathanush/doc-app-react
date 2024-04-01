3daaf9ca76bc2672461e35f9fb7b11945410cdc8
Thanush A  A <aathanush@gmail.com>
2024-04-01 16:52:24 +0530

    completed pagination in appointments
    comple

--- src/patient/appointment-card.jsx
+++ src/patient/appointment-card.jsx
@@ -1,12 +1,15 @@
+
 export default function AppointmentCard( {obj} ) {
     return (
-        <div>
-            <div>
+        <div className="appointment-card">
+            <div className="appointment-details">
+                <img src="./src/assets/appointment.jpg" alt="" />
                 <h3>{obj.date}</h3>
                 <p>{obj.timing}</p>
                 <p>{obj.description}</p>
-                {obj.status ? <p style={{color: "green"}}>Confirmed</p> : <p style={{color: "red"}}>Not Confirmed</p>}
+                {obj.status ? (<p className="confirmed">Confirmed</p>) : (<p className="not-confirmed">Not Confirmed</p>)}
             </div>
         </div>
+
     );
 }
--- src/patient/patient-appointment-view.jsx
+++ src/patient/patient-appointment-view.jsx
@@ -1,31 +1,56 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import AppointmentCard from './appointment-card';
+import ReactPaginate from 'react-paginate';
+import { AiFillLeftCircle, AiFillRightCircle } from "react-icons/ai"; // icons form react-icons
+import { IconContext } from "react-icons"; // for customizing icons
 
-export default function PatientDoctorView() {
+export default function PatientAppointmentView() {
+    const [page, setPage] = useState(0); 
     const [appList, setappList] = useState([]);
+    const [filterappList, setfilterappList] = useState([]);
+    const n = 2; 
+
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await axios.get('http://localhost:8090/patient/get_appointments/'+window.sessionStorage.getItem("patientId"));
                 setappList(response.data);
-                console.log(response.data);
+                setfilterappList(appList.filter((item, index) => {
+                    return (index >= page * n) & (index < (page + 1) * n);
+                  }));
+              
+                
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
         };
 
         fetchData();
-    }, []);
+    }, [page]);
 
     return (
-        <div>
-            {appList.map((appointment) => (
-                <>
-                <AppointmentCard obj={appointment} />
-                </>
-            ))}
-        </div>
+        <>
+        {filterappList.map((appointment) => (
+            <AppointmentCard key={appointment.id} obj={appointment} />
+        ))}
+
+        {/* Pagination controls */}
+        <ReactPaginate
+        className='pagination-container'
+            previousLabel={<AiFillLeftCircle className='page-item left' />}
+            nextLabel={<AiFillRightCircle className='page-item right'/>}
+            breakLabel={'...'}
+            pageCount={Math.ceil(appList.length / n)}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={(selected) => setPage(selected.selected)}
+            containerClassName={'pagination'}
+            activeClassName={'active'}
+        />
+    </>
+
+        
     );
 }
